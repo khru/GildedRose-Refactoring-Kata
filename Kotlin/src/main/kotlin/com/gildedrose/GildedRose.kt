231bd3efc4960fb7ae6b67e1636491badbcf1623
Marc Mauri Alloza <marc.mauri@codurance.com>
2022-04-29 18:07:49 +0200

    Extract Aged Brie Case

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,21 +3,18 @@ package com.gildedrose
 class GildedRose(var items: Array<Item>) {
 
     private val BACK_STAGE_TO_A_TAFKAL80ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert"
+    private val AGED_BRIE = "Aged Brie"
 
     fun updateQuality() {
         for (i in items.indices) {
             if (items[i].name == BACK_STAGE_TO_A_TAFKAL80ETC_CONCERT) {
                 `Backstage passes to a TAFKAL80ETC concert`(i)
+            } else if (items[i].name == AGED_BRIE) {
+                `Aged Brie`(i)
             } else {
-                if (items[i].name == "Aged Brie") {
-                    if (qualityIsLessthanFifty(i)) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                } else {
-                    if (items[i].quality > 0) {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                            items[i].quality = items[i].quality - 1
-                        }
+                if (items[i].quality > 0) {
+                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                        items[i].quality = items[i].quality - 1
                     }
                 }
 
@@ -26,20 +23,14 @@ class GildedRose(var items: Array<Item>) {
                 }
 
                 if (items[i].sellIn < 0) {
-                    if (items[i].name != "Aged Brie") {
-                        if (items[i].name != BACK_STAGE_TO_A_TAFKAL80ETC_CONCERT) {
-                            if (items[i].quality > 0) {
-                                if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                    items[i].quality = items[i].quality - 1
-                                }
+                    if (items[i].name != BACK_STAGE_TO_A_TAFKAL80ETC_CONCERT) {
+                        if (items[i].quality > 0) {
+                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                                items[i].quality = items[i].quality - 1
                             }
-                        } else {
-                            items[i].quality = items[i].quality - items[i].quality
                         }
-                    } else { // Aged Brie
-                        if (qualityIsLessthanFifty(i)) {
-                            items[i].quality = items[i].quality + 1
-                        }
+                    } else {
+                        items[i].quality = items[i].quality - items[i].quality
                     }
                 }
             }
@@ -78,6 +69,20 @@ class GildedRose(var items: Array<Item>) {
         items[i].sellIn = items[i].sellIn - 1
 
         qualityIsZeroWhenSellInDatePassedRule(i)
+    }
+
+    private fun `Aged Brie`(i: Int) {
+        if (qualityIsLessthanFifty(i)) {
+            items[i].quality = items[i].quality + 1
+        }
+
+        items[i].sellIn = items[i].sellIn - 1
+
+        if (items[i].sellIn < 0) {
+            if (qualityIsLessthanFifty(i)) {
+                items[i].quality = items[i].quality + 1
+            }
+        }
     }
 
 }
