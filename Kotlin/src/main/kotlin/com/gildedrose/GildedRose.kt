28416b68f973b9edb8b035348e5c67349b262976
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-04-29 17:47:29 +0200

    extract funtion qualityIsLessthanFifty and create constant for BACK_STAGE_TO_A_TAFKAL80ETC_CONCERT

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -2,13 +2,15 @@ package com.gildedrose
 
 class GildedRose(var items: Array<Item>) {
 
+    private val BACK_STAGE_TO_A_TAFKAL80ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert"
+
     fun updateQuality() {
         for (i in items.indices) {
-            if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+            if (items[i].name == BACK_STAGE_TO_A_TAFKAL80ETC_CONCERT) {
                 `Backstage passes to a TAFKAL80ETC concert`(i)
             } else {
                 if (items[i].name == "Aged Brie") {
-                    if (items[i].quality < 50) {
+                    if (qualityIsLessthanFifty(i)) {
                         items[i].quality = items[i].quality + 1
                     }
                 } else {
@@ -25,7 +27,7 @@ class GildedRose(var items: Array<Item>) {
 
                 if (items[i].sellIn < 0) {
                     if (items[i].name != "Aged Brie") {
-                        if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                        if (items[i].name != BACK_STAGE_TO_A_TAFKAL80ETC_CONCERT) {
                             if (items[i].quality > 0) {
                                 if (items[i].name != "Sulfuras, Hand of Ragnaros") {
                                     items[i].quality = items[i].quality - 1
@@ -35,7 +37,7 @@ class GildedRose(var items: Array<Item>) {
                             items[i].quality = items[i].quality - items[i].quality
                         }
                     } else { // Aged Brie
-                        if (items[i].quality < 50) {
+                        if (qualityIsLessthanFifty(i)) {
                             items[i].quality = items[i].quality + 1
                         }
                     }
@@ -45,22 +47,24 @@ class GildedRose(var items: Array<Item>) {
     }
 
     private fun qualityIncreasesWithDateRule(i: Int) {
-        if (items[i].quality < 50) {
+        if (qualityIsLessthanFifty(i)) {
             items[i].quality = items[i].quality + 1
 
             if (items[i].sellIn < 11) {
-                if (items[i].quality < 50) {
+                if (qualityIsLessthanFifty(i)) {
                     items[i].quality = items[i].quality + 1
                 }
             }
 
             if (items[i].sellIn < 6) {
-                if (items[i].quality < 50) {
+                if (qualityIsLessthanFifty(i)) {
                     items[i].quality = items[i].quality + 1
                 }
             }
         }
     }
+
+    private fun qualityIsLessthanFifty(i: Int) = items[i].quality < 50
 
     private fun qualityIsZeroWhenSellInDatePassedRule(i: Int) {
         if (items[i].sellIn < 0) {
