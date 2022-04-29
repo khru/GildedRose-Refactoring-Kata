16a54d4010d6abc6a7d6878ad8cfd8833c25513f
Marc Mauri Alloza <marc.mauri@codurance.com>
2022-04-29 17:28:51 +0200

    Isolate Backestage pases case

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,38 +4,40 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
-            if (items[i].name == "Aged Brie" || items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    `Backstage passes to a TAFKAL80ETC concert`(i)
-                }
+            if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                `Backstage passes to a TAFKAL80ETC concert`(i)
             } else {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            }
-
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") { // Aged brie
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else { // Aged Brie
+                if (items[i].name == "Aged Brie") {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1
+                    }
+                } else {
+                    if (items[i].quality > 0) {
+                        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                            items[i].quality = items[i].quality - 1
+                        }
+                    }
+                }
+
+                if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                    items[i].sellIn = items[i].sellIn - 1
+                }
+
+                if (items[i].sellIn < 0) {
+                    if (items[i].name != "Aged Brie") {
+                        if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                            if (items[i].quality > 0) {
+                                if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                                    items[i].quality = items[i].quality - 1
+                                }
+                            }
+                        } else {
+                            items[i].quality = items[i].quality - items[i].quality
+                        }
+                    } else { // Aged Brie
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1
+                        }
                     }
                 }
             }
@@ -43,7 +45,9 @@ class GildedRose(var items: Array<Item>) {
     }
 
     private fun `Backstage passes to a TAFKAL80ETC concert`(i: Int) {
-        if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+        if (items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1
+
             if (items[i].sellIn < 11) {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1
@@ -55,6 +59,12 @@ class GildedRose(var items: Array<Item>) {
                     items[i].quality = items[i].quality + 1
                 }
             }
+        }
+
+        items[i].sellIn = items[i].sellIn - 1
+
+        if (items[i].sellIn < 0) {
+            items[i].quality = items[i].quality - items[i].quality
         }
     }
 
